f703834c8e64e26df268671f93bdf42accd15457
Nicholas <nick@ballboards.com>
2025-01-19 11:01:04 -0600

    x

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,5 @@
+export const dynamic = 'force-dynamic';
+
 import { prisma } from '@/lib/db'
 import { UploadDialog } from '@/components/upload-dialog'
 import { ImageGrid } from '@/components/image-grid'
