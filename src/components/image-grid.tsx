6f6cd2ce8b5b4a832f0be3877e7963b7a71e19ce
Nicholas <nick@ballboards.com>
2025-01-19 10:58:20 -0600

    x

--- src/components/image-grid.tsx
+++ src/components/image-grid.tsx
@@ -17,6 +17,7 @@ type Image = {
 }
 
 export function ImageGrid({ images }: { images: Image[] }) {
+    console.log(images)
   return (
     <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
       {images.map((image) => (
